b57d9a09e18b6f5bc20ef26629dd7e4dfe9ae2bf
Carlos Serrano <carlos.serrano@enti.cat>
2019-12-17 11:54:00 +0100

    Ajustes AA8

--- src/AA1_07/Grafos.cpp
+++ src/AA1_07/Grafos.cpp
@@ -8,7 +8,7 @@ int main() {
 	edge ed2 = { 2,3 };
 	edge ed3 = { 1,3 };
 
-	edge ed4 = { 2,2 };
+	edge ed4 = { 2,4 };
 	edge ed5 = { 1,5 };
 
 	//ele.push_back(/*ed1*/std::pair<int, int>(2,3));
@@ -16,10 +16,10 @@ int main() {
 	//ele.push_back(/*ed1*/std::pair<int,int>(1,2));
 
 	ele.push_back(ed1);
-	ele.push_back(ed2);
+	//ele.push_back(ed2);
 	ele.push_back(ed3);
 	ele.push_back(ed4);
-	//ele.push_back(ed4);
+	ele.push_back(ed5);
 
 	GraphD* g = new GraphD(ele);
 
--- src/AA1_07/Grafos.h
+++ src/AA1_07/Grafos.h
@@ -19,20 +19,6 @@ public:
 
 	//2. Constructor a partir de la lista de arcos
 	GraphD(std::vector<edge> el) {
-
-		/*std::vector<edge>::iterator iterator = el.begin();
-		while (iterator != el.end()) {
-			std::vector<edge>::iterator iterator2 = el.begin();
-			std::vector<vertex> v;
-			while (iterator2 != el.end()) {
-				int i = iterator->first;
-				if (i == iterator2->first) { v.push_back(iterator2->second); } // metemos valores en el vector
-				iterator2++;
-			}
-			g.insert(std::pair<vertex, std::vector<vertex>>(iterator->first, v));
-			iterator++;
-		}*/
-
 		for (int i = 0; i < el.size(); i++) {
 			Insert(el[i]);
 		}
@@ -47,39 +33,19 @@ public:
 		std::map<vertex, std::vector<vertex>>::iterator it2 = g.find(ed.second);
 
 		if (it2 == g.end()) { g[ed.second]; it2 = g.find(ed.second); }
-		/*else*/ if (it != g.end() /*&& it2 != g.end()*/) {
+		if (it != g.end()) {
 
 			std::vector<vertex>::iterator it3;
 			
-			if (g[ed.first].size() == 0) { g[ed.first].push_back(ed.second); it3 = g[ed.first].end(); }
-			else if (g[ed.first].size() == 1 && g[ed.first][0] != ed.second) {
-				bool exists = false;
-				for (int i = 0; i < g[ed.first].size(); i++) { if (g[ed.first][i] == ed.second) exists = true; }
-
-				if (!exists) { g[ed.first].push_back(ed.second); }
+			if (g[ed.first].size() == 0) { g[ed.first].push_back(ed.second); it3 = g[ed.first].end(); } // si está vacío
+			else if (g[ed.first].size() == 1 && g[ed.first][0] != ed.second) { // si sólo tiene un elemento
+				
+				g[ed.first].push_back(ed.second);
 				it3 = g[ed.first].end();
 			}
 			else { it3 = std::find(g[ed.first].begin(), g[ed.first].end(), ed.second); }
 
 			if (it3 != g[ed.first].end() && ed.second !=*it3) { g[ed.first].push_back(ed.second); }
-
-			/*std::vector<vertex>::iterator v_it = it->second.begin();
-
-			if (it->second.size() == 1) {
-
-				if (*v_it == ed.second) {}
-				else { it->second.push_back(ed.second); return; }
-
-			}
-
-			bool exists = false;
-			for (; v_it != it->second.end(); v_it++) {
-
-				if (*v_it == ed.second) { exists = true; }
-
-			}
-			if (!exists) { it->second.push_back(ed.second); }*/
-
 
 			}
 			else {
@@ -114,39 +80,6 @@ public:
 			if (it3 != g[ed.first].end() && ed.second == *it3) { g[ed.first].erase(it3); }
 
 		}
-		
-
-
-		/*std::map<vertex, std::vector<vertex>>::iterator it = g.find(ed.first);
-		if (it != g.end()) {
-			std::vector<vertex>::iterator v_it = it->second.begin();
-
-			if (it->second.size() == 1) {
-
-				if (*v_it == ed.second) {}
-				else { it->second.erase(v_it); return; }
-
-			}
-
-			for (; v_it != it->second.end(); v_it++) {
-
-				if (*v_it == ed.second) { it->second.erase(v_it); return; }
-
-			}
-
-
-		}*/
-
-		//std::map<vertex, std::vector<vertex>>::iterator it = g.find(ed.first);
-		
-		/*if (it != g.end()) {
-
-			std::vector<vertex>::iterator it3 = std::find(g[ed.first].begin(), g[ed.first].end(), ed.second);
-			if (it3 != g[ed.first].end()) { 
-					g[ed.first].erase(it3);
-				
-			}
-		}*/
 	}
 
 	//5. Verifica si existe un camino desde el vértice initial
@@ -155,6 +88,7 @@ public:
 	//6. Verifica si existe un camino desde el vértice initial hasta el vértice final, si no existe devuelve false
 	//y la vlist estará vacía.Si existe, devuelve true y construye en vlist la lista
 	bool Path(vertex initial, vertex final, std::forward_list<vertex> vlist);
+
 
 	//7. Imprime por pantalla la lista de los arcos representando al grafo /////
 	void Print() {
